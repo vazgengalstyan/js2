e4fd5f6142142b7c9f262ba0962b6c2e89ec2be6
unknown <you@example.com>
2022-08-06 02:27:48 +0400

    first homework about querySelectors

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
 
-
-let colors = ['red', 'yellow', 'green'];
-let elements = document.getElementsByClassName('block');
-let activeColor = 'red';
-
-let changeColor = ()=>{
-    let activeColorIndex = colors.indexOf(activeColor);
-    activeColor = colors[activeColorIndex + 1] !== undefined ? colors[activeColorIndex + 1] : colors[0];
-    if(elements[activeColorIndex + 1]){
-        elements[activeColorIndex + 1].style.backgroundColor = activeColor;
-    }else {
-        elements[0].style.backgroundColor = activeColor;
-    }
-    elements[activeColorIndex].style.backgroundColor = 'white';
-};
+//1.js-ում ստանալ հղումները և ամեն երկրորդի գույնը դարձել կարմիր։
 
 
-setInterval(changeColor,2000);
+let elem = document.getElementsByTagName('a');
+let arr = [];
+
+function start() {
+	for (let i = 0; i < elem.length; i++) {
+ 			elem[i].style.color = 'red'
+            arr.push(elem[i].href);
+	}
+    
+ }  
+
+  start();
+//2.հղումներից ստանալ href-ները և լցնել մասիվի մեջ։  (մասիվը տպել):
+  console.log(arr);
+
+  
+//3.հղումներին ավելացնել ոչ ստանդարտ attribute։ Օրինակ test attribute, ինչ-որ value-ով։
+
+let elements = document.querySelector('.test_1');
+
+elements.setAttribute('test', 'hi');
+console.log(elements);
+console.log(elements.getAttribute('test'));
+
+
+//4. հղումներին ավելացնել ստանդարտ target attribute _blank value-ով։
 
 
 
-let element = document.querySelector('#test_id1');
+let target = Array.from(document.querySelectorAll("a"))
 
-setInterval(()=>{
-    element.hidden = !element.hidden;
-}, 1000);
+for (i in target){
+    target[i].setAttribute("target", "blank")
+}
+console.log(target);
 
+//5.ջնջել href attribute-ը ամեն երկրորդ հղումից։
 
-
-let element2 = document.querySelector('#test_id8');
-element2.setAttribute('test', 'aaa');
-
-let elements2 = document.querySelectorAll('[test]');
-
-console.log(elements2);
-console.log(element2.getAttribute('test'));
+let all_links = document.getElementsByTagName("a");
+function start1() {
+for(let i=0; i<all_links.length; i++){
+    all_links[i].removeAttribute("href");
+   
+}
+}
+start1();
+console.log(all_links);
