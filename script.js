5a9bfcf03be9efd6630c2fcb7ab2d9c643c2000c
Inesa83 <103387219+Inesa83@users.noreply.github.com>
2022-08-06 13:32:48 +0400

    new file

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
-let colors = ['red','green','yellow'];
-let element = document.getElementById('test_id');
-let elements = document.getElementById('test_id1');
-let elements1 = document.getElementById('test_id2');
-let selectedColorIndex = 1
 
 
-let changeColor = ()=>{
-	
-	if(selectedColorIndex < colors.length - 1){
-		selectedColorIndex++
-	}else{
-		selectedColorIndex = 0
+let elem = document.querySelectorAll('a')
+let arr = [elem];
+function link(arr){
+	for(let i = 0;i<arr.length;i++){
+		if(i%2  !==0){
+			arr[i].style.color = 'red'
+		}
 	}
-	element.style.backgroundColor = colors[selectedColorIndex]
-	selectedColorIndex = (selectedColorIndex +1) % colors.length
 }
-setInterval(changeColor,1000);
-
-function changColors (){
-	elements.style.backgroundColor = colors[selectedColorIndex];
-	selectedColorIndex = (selectedColorIndex +1) % colors.length
-}
-setInterval( changColors,1000);
-
-
-function  changColors1 (){
-	elements1.style.backgroundColor = colors[selectedColorIndex];
-	selectedColorIndex = (selectedColorIndex +1) % colors.length
-}
-setInterval( changColors1,1000)
+console.log(link(elem))
