68736901977449974118e6c3b3674ba1eb30d073
Inesa83 <103387219+Inesa83@users.noreply.github.com>
2022-08-06 14:51:57 +0400

    new files

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
 
 
+
 let elem = document.querySelectorAll('a')
 let arr = [elem];
-function link(arr){
-	for(let i = 0;i<arr.length;i++){
+function getLink(arr){
+	for(let i = 0;i < elem.length; i++){
 		if(i%2  !==0){
-			arr[i].style.color = 'red'
+			elem[i].style.color = 'red'
 		}
 	}
 }
-console.log(link(elem))
+getLink()
+
+
+
+
+let array = []
+function getHref(elem){
+    let array = []
+	for(let i = 0;i<elem.length;i++){
+    arr.push(elem[i].getAtribute('href'))
+}
+return array
+}
+console.log(arr)
+
+
+
+function getAtribut(elem){
+    
+	for(let i = 0;i<elem.length;i++){
+    elem[i].setAttribute("test","Hy")
+	console.log(elem[i])
+}
+
+}
+
+
+getAtribut(elem)
+
+
+function dell(elem){
+    
+	for(let i = 0;i<elem.length;i++){
+   if(i%2 !==0){
+	elem[i].removeAttribute('href')
+}
+     console.log(elem[i])
+	}
+
+
+}
+dell(elem)
+
+
+getAtribut(elem)
