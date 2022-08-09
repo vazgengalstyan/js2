fce8f2985fe1beaf09a2240ce46ad4252102e9ef
GoharKhachatryann <110253530+GoharKhachatryann@users.noreply.github.com>
2022-08-09 13:48:13 +0400

    Update homework.js

--- homework.js
+++ homework.js
@@ -1,1 +1,36 @@
-jdb
+let data = {
+    "Рыбы":{
+        "форелъ":{},
+        "лососъ":{}
+    },
+    "Деревя":{
+        "Огромные":{
+            "секвоя":{},
+            "дуб":{}
+        },
+        "Цветковые":{
+            "яблоня":{},
+            "магнолия":{}
+        }
+    }
+}
+
+
+function tree(data){
+    let ul = document.createElement("ul");
+
+        for(let  key in data){
+        let li = document.createElement("li");
+        li.innerHTML = key;
+
+        let list = tree(data[key]);
+        li.append(list);
+        ul.append(li);
+        list.style.color = "red"
+       
+        }
+    
+        return ul;
+
+}
+document.body.prepend(tree(data));
