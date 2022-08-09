a347b3cb90c5c7bbf5b7f788926f5b32ffe2e4d5
Inesa83 <103387219+Inesa83@users.noreply.github.com>
2022-08-09 13:37:52 +0400

    my new file

--- script.js
+++ script.js
@@ -52,3 +52,21 @@ function getList(data){
 }
 
 document.body.prepend(getList(data));
+
+
+let arr = [
+    { element: 'p', content: 'test text', id: 1 },
+    { element: 'div', class: 'block', id: 2 },
+    { element: 'input', value: 'test'}
+]
+
+
+arr.forEach(function(item){
+let list = document.createElement(item.element);
+for(let key in item){
+list.innerText = item[key]
+list.setAttribute(key,item[key])
+
+}
+document.body.append(list)
+})
