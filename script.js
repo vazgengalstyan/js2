7f9e18543ca6af1931e3ff8336cb3238a259fc84
unknown <xlxatyanjulia@gmail.com>
2022-08-11 01:37:53 +0400

    calculator homework

--- script.js
+++ script.js
@@ -1,61 +1,37 @@
-let data = {
-  "Рыбы": {
-    "форель": {},
-    "лосось": {}
-  },
-  "Деревья": {
-    "Огромные": {
-      "секвойя": {},
-      "дуб": {}
-    },
-    "Цветковые": {
-      "яблоня": {},
-      "магнолия": {}
-    }
-  }
-};
-
-let container = document.querySelector('#container');
-
-let myFunc = (container,data) => {
-    let ul = document.createElement('ul');
-    container.append(ul);
-    for (let key in data) {
-        let li = document.createElement('li');
-        li.innerText = key;
-        ul.append(li);
-        if(Object.keys(data[key]).length) {
-            console.log(Object.keys(data[key]))
-            myFunc(ul, data[key]);
-        }
-    }
-}
-
-myFunc(container,data);
-
-
-
-let array = [
-    {element: 'p', content: 'test text', id: 1},
-    {element: 'div', class: 'block', id: 2},
-    {element: 'input', value: 'test'}
-];
-
-let getElements = (arr) => {
-    for (let item of arr) {
-        let element = document.createElement(item.element);
-        if (item.content) {
-            element.innerText = item.content;
-        }
-        for (let key in item) {
-            if (key === 'element' || key === 'content') {
-                continue;
-            } else {
-                element.setAttribute(key, item[key]);
+let calculateNumber = () => {
+    let firstInput = Number(document.querySelector('#first_input').value);
+    let secondInput = document.querySelector('#second_input').value;
+    let thirdInput = Number(document.querySelector('#third_input').value);
+    let result;
+    switch(secondInput) {
+        case '+':
+           result = firstInput + thirdInput;
+           break;
+        case '-':
+            result = firstInput - thirdInput;
+            break;
+        case '*':
+            result = firstInput * thirdInput;
+            break;
+        case '/':
+            result = firstInput / thirdInput;
+            if(thirdInput === 0) {
+                alert('Is not divitible by 0');
+                return null;
             }
-        }
-        document.body.append(element);
+            break;
+        case '%':
+            result = firstInput % thirdInput;
+            break;
+        default:
+            alert('Please enter only +, -, /, * operators');
+            return null;
+    }
+    if(result !== null) {
+        alert(firstInput + secondInput + thirdInput + '=' + result);
     }
 }
 
-getElements(array);
+
+
+
