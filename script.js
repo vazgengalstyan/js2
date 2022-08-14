833bc8b8f30ad2aa2037f16aca21c4003056b1e6
Inesa83 <103387219+Inesa83@users.noreply.github.com>
2022-08-15 01:35:36 +0400

    new

--- script.js
+++ script.js
@@ -1,72 +1,20 @@
-let data = {
-    "Рыбы":{
-        "форелъ":{},
-        "лососъ":{}
-    },
-    "Деревя":{
-        "Огромные":{
-            "секвоя":{},
-            "дуб":{}
-        },
-        "Цветковые":{
-            "яблоня":{},
-            "магнолия":{}
-        }
-    }
-}
 
-/*let data = {
-    Fish: {
-      trout: {},
-      salmon: {}
-    },
-  
-    Tree: {
-      Huge: {
-        sequoia: {},
-        oak: {}
-      },
-      Flowering: {
-        "apple tree": {},
-        magnolia: {}
-      }
-    }
-  };*/
+ let elem = document.getElementById('block')
+ let elems = document.getElementById('container')
+ 
+/*elems.addEventListener("click",function(event){
+	 elem.style.left = event.clientX + 'px'
+	 elem.style.top = event.clientY + 'px'
+ })*/
+ 
+ /*elems.addEventListener("click",function(event){
+	 elem.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+})*/
 
-function getList(data){
-    let ul = document.createElement("ul");
+/*elems.addEventListener("mousemove",function(event){
+	elem.style.left = event.clientX + 'px';
+	elem.style.top = event.clientY + 'px';
+	
+	 
+}) */
 
-    for(let  key in obj){
-        let li = document.createElement("li");
-        li.innerText = key;
-
-        let list = getList(obj[key]);
-        li.append(list);
-        ul.append(li);
-        list.style.color = "red"
-
-    }
-    return ul;
-
-
-}
-
-document.body.prepend(getList(data));
-
-
-let arr = [
-    { element: 'p', content: 'test text', id: 1 },
-    { element: 'div', class: 'block', id: 2 },
-    { element: 'input', value: 'test'}
-]
-
-
-arr.forEach(function(item){
-let list = document.createElement(item.element);
-for(let key in item){
-list.innerText = item[key]
-list.setAttribute(key,item[key])
-
-}
-document.body.append(list)
-})
