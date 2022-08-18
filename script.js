76aebfa555d6f5b58068fca05b71319b1f38336e
unknown <xlxatyanjulia@gmail.com>
2022-08-18 16:32:11 +0400

    Pagination

--- script.js
+++ script.js
@@ -1,65 +1,63 @@
-//1.mknik@ sharjeluc element@ het@ petq e sharjvi.
-let firstContainer = document.querySelector('#first_container');
+let arr = [];
 
-// let myFunc1 = (e) => {
-//     firstContainer.style.top = e.clientY + 'px';
-//     firstContainer.style.left = e.clientX + 'px';
-// }
-// document.documentElement.addEventListener('mousemove',myFunc1);
-
-//2.mknik@ elementi vra bereluc petq e element@ haytnvi ekrani patahakan dirqum
-// let myFunc2 = () => {
-//     firstContainer.style.top =(Math.round(Math.random() * (window.innerHeight - firstContainer.offsetHeight))) + 'px';
-//     firstContainer.style.left = (Math.round(Math.random() * (window.innerWidth - firstContainer.offsetWidth))) + 'px';
-// }
-//
-// firstContainer.addEventListener('mouseover', myFunc2);
-
-//3.elementi vra sexmeluc petq e element@ stana random guyn
-
-// let thirdContainer = document.querySelector('#third_small_container');
-//
-// let changeColor = () => {
-//     thirdContainer.style.backgroundColor = `#${Math.round(Math.random() * 16777215).toString(16)}`;
-// }
-//
-// thirdContainer.addEventListener('click',changeColor);
-
-//4.mknikov bdi krananq element@ brnenq u texasharjenq
-
-// let myFunc3 = (e) => {
-//     e.preventDefault();
-//     firstContainer.style.top = e.clientY + 'px';
-//     firstContainer.style.left = e.clientX + 'px';
-// }
-//
-// document.addEventListener('dragover',myFunc3);
-
-//5.ekrani cankacac tex click eneluc element@ bdi haytnvi @dtex
-//
-// let myFunc4 = (e) => {
-//     firstContainer.style.top = e.clientY + 'px';
-//     firstContainer.style.left = e.clientX + 'px';
-// }
-// document.documentElement.addEventListener('click',myFunc4);
-
-//6.stexcel html-um forma (anun, axganun, email, tariq, ser, hasce) nshvac dashterov yev submit sexmeluc tpeq formai
-// arjeqner@ console-um obyekti tesqov
-
-let form = document.querySelector('#main_form');
-let inputArray = document.querySelectorAll('input');
-
-let getValueInputs = (e) => {
-    e.preventDefault();
-    let obj = {};
-    for(let item of inputArray) {
-        if(item.type === 'submit') {
-            continue;
-        };
-        obj[item.name] = item.value;
-    }
-    console.log(obj);
+async function getPosts() {
+    let response = await fetch('https://restcountries.com/v3.1/all', {
+        method: 'GET'
+    }).then(response => response.json());
+    arr = await response.filter(item => item.continents.includes('Asia'));
 }
-form.addEventListener('submit',getValueInputs);
+getPosts();
+
+let table = document.querySelector('table');
+let ul = document.querySelector('ul');
+
+
+let pageCount = 10;
+let count;
+let items = [];
+
+setTimeout(() => {
+    count = Math.ceil(arr.length / pageCount);
+    for(let i = 1; i <= count; i++) {
+        let li = document.createElement('li');
+        ul.append(li);
+        li.innerText = i;
+        items.push(li);
+    }
+
+    for(let item of items) {
+        item.addEventListener('click', function() {
+            let active = document.querySelector('li.active');
+            if(active) {
+                active.classList.remove('active');
+            }
+            this.classList.add('active');
+            let pageNum = +this.innerText;
+            let start = (pageNum - 1) * pageCount;
+            let end = start + pageCount;
+
+            let posts = arr.slice(start,end);
+            table.innerHTML = '';
+            for(let post of posts) {
+                let tr;
+                tr = document.createElement('tr');
+                table.append(tr);
+                let td;
+                td = document.createElement('td');
+                td.innerText = post.continents;
+                tr.append(td);
+
+                td = document.createElement('td');
+                td.innerText = post.capital;
+                tr.append(td)
+
+                td = document.createElement('td');
+                td.innerText = post.timezones;
+                tr.append(td);
+            }
+        })
+    }
+},1500)
+
 
 
