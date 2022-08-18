e00c9b7b3a7278a751e2da61cfe2b109ffad5a3d
Inesa83 <103387219+Inesa83@users.noreply.github.com>
2022-08-18 17:44:33 +0400

    new

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
  let elem = document.getElementById('block')
  let elems = document.getElementById('container')
  
-/*elems.addEventListener("click",function(event){
+elems.addEventListener("click",function(event){
 	 elem.style.left = event.clientX + 'px'
 	 elem.style.top = event.clientY + 'px'
- })*/
+ })
  
  /*elems.addEventListener("click",function(event){
 	 elem.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
